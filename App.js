1e9c03b425ecdeba7b2a652a51479a5a4ecc98c7
ltmtk10-nhom5 <78581880+ltmtk10-nhom5@users.noreply.github.com>
2021-03-05 19:56:10 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React from 'react';                                                                                   
-import Login from './Login';
-import Products from './Products';
+  
+import React from 'react';
+import Login from './src/screens/Login';
+import Products from './src/screens/Products';
 
-import RootNativation from './Navigations'
+import RootNativation from './src/navigations';
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <RootNativation></RootNativation>
   );
 }
