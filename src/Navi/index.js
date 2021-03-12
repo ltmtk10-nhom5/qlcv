bff18d06310a633bf434d0073c4a6802696b8883
ltmtk10-nhom5 <78581880+ltmtk10-nhom5@users.noreply.github.com>
2021-03-12 13:19:44 +0700

    Update index.js

--- src/Navi/index.js
+++ src/Navi/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from '../screens/Login';
-import Products from '../screens/Products';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../screens/Login";
+import Products from "../screens/Products";
+import ProductDetail from "../screens/ProductDetail";
 
 const Stack = createStackNavigator();
 
-function  RootNativation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{
-                headerShown: false
-            }}>
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Products" component={Products} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+function RootNativation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Product" component={Products} />
+        <Stack.Screen name="ProductDetail" component={ProductDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default RootNativation;
